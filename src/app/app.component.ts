14303792ef2a4b83f26c6b97f2be5e4d99f33b4f
batabalazs96 <43841279+batabalazs96@users.noreply.github.com>
2021-08-03 19:30:23 +0200

    passing data with event and property binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  currentLoad = 'recipe'
+
+  onNavigate(feature:string){
+    this.currentLoad = feature;
+  }
+
   title = 'CookBook-angular-project';
 }
--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output , EventEmitter} from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,7 +7,14 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes : Recipe[] = [new Recipe('Test Recipe', 'Test Desc', 'https://abudhabitiming.com/wp-content/uploads/2020/01/test-img.jpg')];
+
+  recipes : Recipe[] = [new Recipe('Test Recipe', 'Test Descdsafasdfasdfasdf', 'https://abudhabitiming.com/wp-content/uploads/2020/01/test-img.jpg'), new Recipe('Test Recipe', 'Dog Dog Dog', 'https://abudhabitiming.com/wp-content/uploads/2020/01/test-img.jpg')];
+
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
+  onSelectedRecipe(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 
   constructor() { }
 
